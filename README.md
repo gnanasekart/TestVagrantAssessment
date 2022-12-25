eb86e195dbaaff67078ca2517fac9617dc658b2d
t.gnanasekar <gnanasekar69@hotmail.com>
2022-12-25 08:51:00 +0530

    added feature and step definitions added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TestVagrantAssessment
+# Test Vagrant Assessment - Framework
+
+This Framework is Java based code using Rest Assured Library for parsing the Json data and identifying IPL players details. Based on the requirement.
+
+| Dependency           | Version |
+|----------------------|---------|
+| Rest Assured Library | 5.1.1   |
+| Test NG              |         |
+| Json Simple          |         |
+| Jackson Data binder  |         |
--- src/main/java/org/testvg/testRunner/Step_Runner.java
+++ src/main/java/org/testvg/testRunner/Step_Runner.java
@@ -2,15 +2,13 @@ package org.testvg.testRunner;
 
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {
         "src/main/resources/Features/RCB_Team_Player_Details.feature"},
-        glue = {"src/main/java/org/testvg/StepDefinitions/Player_Details_Step_Definitions.java"},
-        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
-        monochrome = true,
-        tags = "not @ignore")
+        glue = {"org.testvg.StepDefinitions"},
+        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"})
 public class Step_Runner {
 }
